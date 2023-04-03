8297baa8703c061add60fa62053c97890a0eb9ec
Alex Coimbra <75739574+coimbralex@users.noreply.github.com>
2023-04-03 15:12:30 -0300

    Update exe_bkp.sh

--- exe_bkp.sh
+++ exe_bkp.sh
@@ -14,9 +14,6 @@ formato_data=$(date "+%d-%m-%Y")
 #Arquivo onde sera enviado o backup compactado.
 nome_arquivo_bkp="backup-$formato_data.tar.gz"
 
-#Local onde sera armazenado os logs
-arquivo_log="/var/log/backup-diario.log"
-
 ###VERIFICAÇÃO SE O DISCO ESTA MONTADO###
 if ! mountpoint -q -- "$backup_destino"; then
 	printf "[$formato_data] DIRETORIO DESTINO SENDO MONTADO!\n"
@@ -29,6 +26,6 @@ tar -czSpf "$backup_destino/$nome_arquivo_bkp" "$backup_origem"
 	printf "backup realizado com sucesso\n"
 ###MANTER APENAS BACKUPS DOS ULTIMOS 5 DIAS###
 find "$backup_destino" -mtime +5 -delete &
-	sleep 15
-###DESMONTA DIRETORIO APOS 15 SEGUNDOS	
+	sleep 360
+###DESMONTA O DIRETORIO APOS 6 MINUTOS	
 fusermount -u ~/Backups	
